ab1f1b2f008d59bebf0346683c91ffd1852941ef
agil <agil.priyatna7717@gmail.com>
2026-04-27 09:00:32 +0200

    feat: add Vercel Analytics integration to track user interactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { HelmetProvider } from "react-helmet-async";
+import { Analytics } from "@vercel/analytics/react";
 import { domAnimation, LazyMotion, MotionConfig } from "framer-motion";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import Index from "./pages/Index";
@@ -9,6 +10,7 @@ const App = () => (
       <LazyMotion features={domAnimation}>
         <LanguageProvider>
           <Index />
+          <Analytics />
         </LanguageProvider>
       </LazyMotion>
     </MotionConfig>
